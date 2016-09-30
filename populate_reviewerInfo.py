c10135c954f8a9ff60dcf3f5b171c265ab9b71de
Rakshitha <rakshitha03@gmail.com>
2016-09-29 23:02:37 -0400

    Pep8 changes and adding the iclr_accepted_reviewers list

--- populate_reviewerInfo.py
+++ populate_reviewerInfo.py
@@ -1,26 +1,27 @@
-import openreview
+# import openreview
 import dblp
-import json
+
+
 # Let's start by initializing the Client with our username, password, and base URL
-openreview_client = openreview.Client(username='aditya.srinivasan.is@gmail.com', password='12345678', baseurl='http://openreview.net')
-
+# openreview_client = openreview.Client(username='rbhat@cs.umass.edu', password='1234567890',
+#                                       baseurl='http://openreview.net')
 # Get the list of accepted reviewers
-reviewers = openreview_client.get_group('ICLR.cc/2017/conference/reviewers')
-#print reviewers
+# reviewers = openreview_client.get_group('ICLR.cc/2017/conference/reviewers')
+# print reviewers
 
+# For the time being we are using the csv file given my melisa. Later we need
+# to get reviewers from here.
 reviewersInfo = {}
 
-#reading csv file
-fp = open("../data/iclr_accepted_reviewers.csv")
+# reading csv file
+fp = open("./iclr_accepted_reviewers.csv")
 for eachLine in fp.readlines():
     firstName, lastName, emailID = eachLine.split(',')
-    name = firstName+" "+lastName
+    name = firstName + " " + lastName
     if name not in reviewersInfo:
-	reviewersInfo[name] = {'emailID':emailID.strip()}
-	authors = dblp.search(name)
-	if authors:	
-		reviewersInfo[name]['publications'] = authors[0].publications 
+        reviewersInfo[name] = {'emailID': emailID.strip()}
+        authors = dblp.search(name)
+        if authors:
+            reviewersInfo[name]['publications'] = authors[0].publications
 
 print reviewersInfo['Marc Lanctot']
-
- 
